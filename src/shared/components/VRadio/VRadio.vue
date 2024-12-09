e87071eb36e67f866f65e804c29725fbb54b59b8
Denis Yurovsky <moon.wave578@gmail.com>
2024-12-09 14:05:04 +0300

    feat: v-radio custom button

--- src/shared/components/VRadio/VRadio.vue
+++ src/shared/components/VRadio/VRadio.vue
@@ -9,6 +9,7 @@
         class="v-radio__input"
         v-model="model"
       />
+      <div class="v-radio__alter">&#10004;</div>
       {{ label }}
     </label>
   </div>
@@ -16,7 +17,7 @@
 
 <script setup lang="ts">
 import type { IVRadioModelValue, IVRadioProps } from '@/shared/components/VRadio/types'
-import { computed, useAttrs } from 'vue'
+import { computed, useAttrs, watch } from 'vue'
 import { useListeners } from '@/shared/composables'
 
 const props = defineProps<IVRadioProps>()
@@ -24,9 +25,13 @@ const model = defineModel<IVRadioModelValue>({ required: true })
 const attrs = useAttrs()
 const listeners = useListeners(attrs)
 
+watch(model, () => {
+  console.log(model.value)
+})
 const classes = computed(() => [
   {
-    'v-radio--disabled': props.disabled
+    'v-radio--disabled': props.disabled,
+    'v-radio--checked': props.value === model.value
   }
 ])
 </script>
@@ -34,10 +39,37 @@ const classes = computed(() => [
 <style lang="scss" scoped>
 .v-radio {
   color: var(--color-black);
+  position: relative;
+  overflow: hidden;
+
+  &__label {
+    display: flex;
+    gap: 8px;
+  }
+  &__input {
+    position: absolute;
+    top: -20px;
+  }
+  &__alter {
+    width: 20px;
+    height: 20px;
+    border: 1px solid var(--color-teal);
+    color: var(--color-white);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &--checked {
+    .v-radio__alter {
+      background-color: var(--color-teal);
+    }
+  }
 
   &--disabled {
     .v-radio__input,
-    .v-radio__label {
+    .v-radio__label,
+    .v-radio__alter {
       cursor: not-allowed;
     }
 
